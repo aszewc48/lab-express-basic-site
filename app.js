c4290891ef9a6356286ecc89f90afea3d69e5554
Adam Szewczyk <aszewczyk1990@hotmail.com>
2022-09-07 11:26:23 -0400

    done

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+app.use(express.static('public'))
+
+app.get('/', function(req,res) {
+    res.sendFile(__dirname + "/views/index.html")
+})
+app.get('/about', function(req,res) {
+    res.sendFile(__dirname + "/views/about.html")
+})
+app.get('/works', function(req,res) {
+    res.sendFile(__dirname + "/views/works.html")
+})
+
+app.listen(3000)
